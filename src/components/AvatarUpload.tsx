fb9a86f682fae9a540c11cf57eda670012726290
SavioNicodemos <nicodemosgcosta@gmail.com>
2024-02-03 01:24:19 +0000

    fix AvatarUpload component

--- src/components/AvatarUpload.tsx
+++ src/components/AvatarUpload.tsx
@@ -4,37 +4,31 @@ import { AppError } from '@utils/AppError';
 import { handleError } from '@utils/handleError';
 import * as FileSystem from 'expo-file-system';
 import * as ImagePicker from 'expo-image-picker';
-import {
-  Box,
-  FormControl,
-  IIconButtonProps,
-  Icon,
-  IconButton,
-} from 'native-base';
+import { Box, FormControl, IBoxProps, Icon, IconButton } from 'native-base';
 import { useState } from 'react';
 import { UserPhoto } from './UserPhoto';
 
-type Props = IIconButtonProps & {
-  value: IImageUpload;
+type Props = IBoxProps & {
+  value: ImageProp;
   onChange: (file: IImageUpload) => void;
   errorMessage?: string;
 };
-
-const PHOTO_SIZE = 24;
 
 export function AvatarUpload({
   errorMessage = '',
   value,
   onChange = () => {},
+  size = 24,
+  ...props
 }: Props) {
-  const [image, setImage] = useState<IImageUpload>(value);
+  const [image, setImage] = useState<ImageProp>(value);
 
   const handleUserPhotoSelect = async () => {
     try {
       const photoSelected = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.Images,
         quality: 1,
-        aspect: [4, 3],
+        aspect: [1, 1],
         allowsEditing: true,
       });
 
@@ -72,11 +66,12 @@ export function AvatarUpload({
       handleError(error);
     }
   };
+
   return (
     <FormControl isInvalid={!!errorMessage} mb={4} alignItems="center">
-      <Box mt="4" width={PHOTO_SIZE} height={PHOTO_SIZE}>
+      <Box mt="4" width={size} height={size} {...props}>
         <UserPhoto
-          size={PHOTO_SIZE}
+          size={Number(size)}
           imageLink={image?.path}
           isExternalImage={image?.isExternal}
         />
@@ -107,3 +102,8 @@ export function AvatarUpload({
     </FormControl>
   );
 }
+
+type ImageProp = {
+  isExternal: boolean;
+  path: string;
+};
