0f30d21e4a6848ea35e3e7a4f66c0f9c09f94cb8
SavioNicodemos <nicodemosgcosta@gmail.com>
2024-03-10 13:58:41 +0000

    chore: rename project

--- src/storage/storageConfig.ts
+++ src/storage/storageConfig.ts
@@ -1,4 +1,4 @@
-const USER_STORAGE = '@letssolve:user';
-const AUTH_STORAGE = '@letssolve:token';
+const USER_STORAGE = '@solveit:user';
+const AUTH_STORAGE = '@solveit:token';
 
 export { AUTH_STORAGE, USER_STORAGE };
